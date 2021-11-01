63e033f089080f47d47fca4533491bd36eb3e10a
Erfan <erfanhanifezade@gmail.com>
2021-11-01 21:39:25 +0330

    slider js and etc

---
+++ static/js/slider.js
@@ -0,0 +1,10 @@
+const sliders = document.querySelectorAll(".slider")
+
+function InitSlider() {
+    // setInterval(()=> {
+    //     console.log(sliders)
+    // } , 3000)
+    console.log(sliders)
+}
+
+InitSlider()
