9e32fc2a41dc9aeaaa696c9f194899c582cb5c74
Erfan <erfanhanifezade@gmail.com>
2021-11-26 22:04:46 +0330

    create migration for database neccesary tables

--- migrations/20211126182131_create_seed_rooms_tables.postgres.up.sql
+++ migrations/20211126182131_create_seed_rooms_tables.postgres.up.sql
@@ -1,2 +1,3 @@
-insert into public.rooms (room_name , created_at , updated_at)
-values ("General's quarters" , "2021-11-26" , "2021-11-26") ("Major's suite" , "2021-11-26" , "2021-11-26");
+insert into public.rooms (room_name , created_at , updated_at) values
+("General's quarters" , "2021-11-26 00:00:00.000" , "2021-11-26 00:00:00.000",
+("Major's suite" , "2021-11-26 00:00:00.000" , "2021-11-26 00:00:00.000");
---
+++ migrations/20211126183103_create_seed_restrictions_tables.postgres.up.sql
@@ -0,0 +1,3 @@
+insert into public.restrictions (restriction_name , created-at , updated-at) values
+("reservation" , "2021-11-26 00:00:00.000" , "2021-11-26 00:00:00.000"),
+("Owner block" , "2021-11-26 00:00:00.000" , "2021-11-26 00:00:00.000");
