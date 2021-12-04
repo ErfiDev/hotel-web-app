bedce1843414caa75beb49f7094205aa9b6e45cb
Erfan <erfanhanifezade@gmail.com>
2021-12-04 19:56:44 +0330

    better allReservation func

--- repository/dbrepo/dbrepo.go
+++ repository/dbrepo/dbrepo.go
@@ -3,11 +3,12 @@ package dbrepo
 import (
 	"context"
 	"database/sql"
+	"time"
+
 	"github.com/erfidev/hotel-web-app/config"
 	"github.com/erfidev/hotel-web-app/models"
 	"github.com/erfidev/hotel-web-app/repository"
 	"golang.org/x/crypto/bcrypt"
-	"time"
 )
 
 type postgresDbRepo struct {
@@ -15,15 +16,15 @@ type postgresDbRepo struct {
 	DB  *sql.DB
 }
 
-func NewPostgresRepo(connection *sql.DB ,app *config.AppConfig) repository.DatabaseRepository {
+func NewPostgresRepo(connection *sql.DB, app *config.AppConfig) repository.DatabaseRepository {
 	return &postgresDbRepo{
-		App : app,
-		DB: connection,
+		App: app,
+		DB:  connection,
 	}
 }
 
-func (psdb postgresDbRepo) InsertReservation(reservation models.Reservation) (int , error) {
-	ctx , cancel := context.WithTimeout(context.Background() , 3 * time.Second)
+func (psdb postgresDbRepo) InsertReservation(reservation models.Reservation) (int, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
 	statement := `insert into reservations (first_name, last_name, email, phone, start_date, end_date,
@@ -31,7 +32,7 @@ func (psdb postgresDbRepo) InsertReservation(reservation models.Reservation) (in
 `
 	var newId int
 
-	err :=psdb.DB.QueryRowContext(
+	err := psdb.DB.QueryRowContext(
 		ctx,
 		statement,
 		reservation.FirstName,
@@ -42,26 +43,26 @@ func (psdb postgresDbRepo) InsertReservation(reservation models.Reservation) (in
 		reservation.EndDate,
 		reservation.RoomId,
 		time.Now(),
-		time.Now(), 
+		time.Now(),
 	).Scan(&newId)
-	
+
 	if err != nil {
-		return 0 , err
+		return 0, err
 	}
-	
-	return newId , nil
+
+	return newId, nil
 }
 
 func (psdb postgresDbRepo) InsertRoomRestriction(roomRestriction models.RoomRestriction) error {
-	ctx , cancel := context.WithTimeout(context.Background() , 3 * time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
 	statement := `insert into room_restrictions (start_date,end_date,
 				room_id, reservation_id , restriction_id , created_at , updated_at)
 				values ($1 , $2 , $3, $4, $5, $6, $7)`
 
-	_ , err := psdb.DB.ExecContext(
-		ctx ,
+	_, err := psdb.DB.ExecContext(
+		ctx,
 		statement,
 		roomRestriction.StartDate,
 		roomRestriction.EndDate,
@@ -79,8 +80,8 @@ func (psdb postgresDbRepo) InsertRoomRestriction(roomRestriction models.RoomRest
 	return nil
 }
 
-func (psdb postgresDbRepo) SearchAvailability(roomId int ,start , end time.Time) (bool , error) {
-	ctx , cancel := context.WithTimeout(context.Background() , 3 * time.Second)
+func (psdb postgresDbRepo) SearchAvailability(roomId int, start, end time.Time) (bool, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
 	var numRows int
@@ -97,18 +98,18 @@ func (psdb postgresDbRepo) SearchAvailability(roomId int ,start , end time.Time)
 	)
 	err := row.Scan(&numRows)
 	if err != nil {
-		return false , err
+		return false, err
 	}
 
 	if numRows == 0 {
-		return true , nil
+		return true, nil
 	}
 
-	return false , nil
+	return false, nil
 }
 
-func (psdb postgresDbRepo) SearchAvailabilityForAllRooms(start , end time.Time) ([]models.Room , error) {
-	ctx , cancel := context.WithTimeout(context.Background() , 3 * time.Second)
+func (psdb postgresDbRepo) SearchAvailabilityForAllRooms(start, end time.Time) ([]models.Room, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
 	statement := `select id , room_name from rooms
@@ -116,9 +117,9 @@ func (psdb postgresDbRepo) SearchAvailabilityForAllRooms(start , end time.Time) 
 
 	var rooms []models.Room
 
-	rows , err := psdb.DB.QueryContext(ctx , statement , start,end)
+	rows, err := psdb.DB.QueryContext(ctx, statement, start, end)
 	if err != nil {
-		return rooms , err
+		return rooms, err
 	}
 
 	for rows.Next() {
@@ -129,31 +130,30 @@ func (psdb postgresDbRepo) SearchAvailabilityForAllRooms(start , end time.Time) 
 		)
 
 		if err != nil {
-			return rooms , err
+			return rooms, err
 		}
 
-		rooms = append(rooms , room)
+		rooms = append(rooms, room)
 	}
 
 	if err = rows.Err(); err != nil {
-		return rooms , err
+		return rooms, err
 	}
 
-
-	return rooms , nil
+	return rooms, nil
 }
 
-func (psdb postgresDbRepo) FindRoomById(id int) (models.Room,error){
-	ctx , cancel := context.WithTimeout(context.Background() , 3 * time.Second)
+func (psdb postgresDbRepo) FindRoomById(id int) (models.Room, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
 	statement := `select id, room_name, updated_at, created_at from rooms where id = $1`
 
 	var room models.Room
 
-	row := psdb.DB.QueryRowContext(ctx, statement , id)
+	row := psdb.DB.QueryRowContext(ctx, statement, id)
 	if err := row.Err(); err != nil {
-		return room , err
+		return room, err
 	}
 
 	err := row.Scan(
@@ -163,44 +163,43 @@ func (psdb postgresDbRepo) FindRoomById(id int) (models.Room,error){
 		&room.CreatedAt,
 	)
 	if err != nil {
-		return room , err
+		return room, err
 	}
 
-	return room , nil
+	return room, nil
 }
 
-func (psdb postgresDbRepo) InsertUser(user models.User) (bool , error) {
-	ctx , cancel := context.WithTimeout(context.Background() , 3 * time.Second)
+func (psdb postgresDbRepo) InsertUser(user models.User) (bool, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
-
 
 	query := `
 	insert into users (first_name , last_name , email , password , access_level) 
 	values ($1 , $2 , $3 , $4 , $5);
 	`
 
-	hashPass , err := bcrypt.GenerateFromPassword([]byte(user.Password) , 10)
+	hashPass, err := bcrypt.GenerateFromPassword([]byte(user.Password), 10)
 	if err != nil {
-		return false , err
+		return false, err
 	}
 
-	_ , err = psdb.DB.ExecContext(ctx , query ,
-		user.FirstName ,
-		user.LastName ,
-		user.Email ,
-		hashPass ,
+	_, err = psdb.DB.ExecContext(ctx, query,
+		user.FirstName,
+		user.LastName,
+		user.Email,
+		hashPass,
 		user.AccessLevel,
 	)
 
 	if err != nil {
-		return false , err
+		return false, err
 	}
 
-	return true , nil
+	return true, nil
 }
 
-func (psdb postgresDbRepo) GetUserById(id int) (models.User , error) {
-	ctx , cancel := context.WithTimeout(context.Background() , 3 * time.Second)
+func (psdb postgresDbRepo) GetUserById(id int) (models.User, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
 	query := `
@@ -210,7 +209,7 @@ func (psdb postgresDbRepo) GetUserById(id int) (models.User , error) {
 
 	var user models.User
 
-	row := psdb.DB.QueryRowContext(ctx , query , id)
+	row := psdb.DB.QueryRowContext(ctx, query, id)
 	err := row.Scan(
 		&user.ID,
 		&user.FirstName,
@@ -221,14 +220,14 @@ func (psdb postgresDbRepo) GetUserById(id int) (models.User , error) {
 	)
 
 	if err != nil {
-		return models.User{} , err
+		return models.User{}, err
 	}
 
-	return user , nil
+	return user, nil
 }
 
-func (psdb postgresDbRepo) Authenticate(email , password string) (int, bool , string) {
-	ctx , cancel := context.WithTimeout(context.Background() , 3 * time.Second)
+func (psdb postgresDbRepo) Authenticate(email, password string) (int, bool, string) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
 	query := `
@@ -239,38 +238,38 @@ func (psdb postgresDbRepo) Authenticate(email , password string) (int, bool , st
 	var pass string
 	var id int
 
-	row := psdb.DB.QueryRowContext(ctx , query , email)
+	row := psdb.DB.QueryRowContext(ctx, query, email)
 
-	err := row.Scan(&id , &pass)
+	err := row.Scan(&id, &pass)
 	if err != nil {
-		return 0 ,false , "can't find user with this email!"
+		return 0, false, "can't find user with this email!"
 	}
 
 	// check passwords
-	hashError := bcrypt.CompareHashAndPassword([]byte(pass) , []byte(password))
+	hashError := bcrypt.CompareHashAndPassword([]byte(pass), []byte(password))
 	if hashError == bcrypt.ErrMismatchedHashAndPassword {
-		return 0 ,false , "mismatched hash and password"
+		return 0, false, "mismatched hash and password"
 	} else if hashError != nil {
-		return 0 ,false , "unexpected error"
+		return 0, false, "unexpected error"
 	}
 
-	return id , true , ""
+	return id, true, ""
 }
 
-func (psdb postgresDbRepo) UpdateUser(user models.User) (bool , error) {
-	ctx , cancel := context.WithTimeout(context.Background() , 3 * time.Second)
+func (psdb postgresDbRepo) UpdateUser(user models.User) (bool, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
 	query := `
 		update users set first_name = $1, last_name = $2, email = $3, password = $4 , access_level = $5
 	`
 
-	hashPass , err := bcrypt.GenerateFromPassword([]byte(user.Password) , 10)
- 	if err != nil {
-		 return false , err
+	hashPass, err := bcrypt.GenerateFromPassword([]byte(user.Password), 10)
+	if err != nil {
+		return false, err
 	}
 
-	_ , errOnExec := psdb.DB.ExecContext(ctx , query ,
+	_, errOnExec := psdb.DB.ExecContext(ctx, query,
 		user.FirstName,
 		user.LastName,
 		user.Email,
@@ -279,42 +278,52 @@ func (psdb postgresDbRepo) UpdateUser(user models.User) (bool , error) {
 	)
 
 	if errOnExec != nil {
-		return false , errOnExec
+		return false, errOnExec
 	}
 
-	return true , nil
+	return true, nil
 }
 
-func (psdb postgresDbRepo) AllReservations() ([]models.Reservation , error) {
-	ctx , cancel := context.WithTimeout(context.Background() , 3 * time.Second)
+func (psdb postgresDbRepo) AllReservations() ([]models.Reservation, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
 	query := `
-  	select rr.start_date, rr.end_date , re.email
-	from room_restrictions rr
-	left join rooms r on (rr.room_id = r.id) 
-	left join reservations re on (rr.reservation_id = re.id);
+  	select r.start_date, r.end_date , r.email ,
+	r.first_name , r.last_name , r.phone , r.created_at,
+	r.updated_at , rm.room_name , rm.id
+	from reservations r
+	left join rooms rm on (r.room_id = rm.id) 
+	order by r.start_date asc;
 	`
 
 	var reservations []models.Reservation
 
-	rows , err := psdb.DB.QueryContext(ctx , query)
+	rows, err := psdb.DB.QueryContext(ctx, query)
+	defer rows.Close()
 	if err != nil {
-		return reservations , err
+		return reservations, err
 	}
 
 	for rows.Next() {
 		res := models.Reservation{}
-		_ = rows.Scan(
+		err := rows.Scan(
 			&res.StartDate,
 			&res.EndDate,
+			&res.Email,
 			&res.FirstName,
+			&res.LastName,
+			&res.Phone,
+			&res.CreatedAt,
+			&res.UpdatedAt,
+			&res.Room.RoomName,
+			&res.RoomId,
 		)
-
-		reservations = append(reservations , res)
+		if err != nil {
+			return reservations, err
+		}
+		reservations = append(reservations, res)
 	}
 
-	return reservations , nil
+	return reservations, nil
 }
-
-
