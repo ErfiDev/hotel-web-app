301f2ddfa4565d71f974fd6ca07574b7a94dc9c2
Erfan <erfanhanifezade@gmail.com>
2021-12-06 14:03:45 +0330

    add new column to reservations table

--- migrations/schema.sql
+++ migrations/schema.sql
@@ -34,7 +34,8 @@ CREATE TABLE public.reservations (
     end_date date NOT NULL,
     room_id integer NOT NULL,
     created_at timestamp without time zone NOT NULL,
-    updated_at timestamp without time zone NOT NULL
+    updated_at timestamp without time zone NOT NULL,
+    processed integer DEFAULT 0 NOT NULL
 );
 
 
