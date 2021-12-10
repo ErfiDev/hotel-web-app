e40647bedef5beabafb98341b4d934d51433c397
Erfan <erfanhanifezade@gmail.com>
2021-12-10 22:13:45 +0330

    create migration for user admin

---
+++ migrations/20211210183735_create_insert_admin_users.postgres.up.sql
@@ -0,0 +1,5 @@
+insert into public.users (first_name, last_name, access_level, email, password, created_at, updated_at)
+values ("Admin" , "Admin" , 2 , "admin@here.com", "$2a$10$/HfzPWj2sW5/SjM6.Sv6J.2ykO6N9ZnfYggZXWDfzkqnJAnT1Dboa",
+"2021-12-10 00:00:00.000", "2021-12-10 00:00:00.000");
+
+-- pass: adminpassword
